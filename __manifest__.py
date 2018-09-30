24f81a6c4ee9c6299018418be9aee19b6d897e77
Pedro A. Gracia Fajardo <lasarux@iMac-de-Pedro.local>
2018-09-30 17:00:05 +0200

    -- add manifest

---
+++ __manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "SyncID",
+
+    'summary': """
+        Module to support external ids to odoo model objects""",
+
+    'description': """
+        Module to support external ids to odoo model objects
+    """,
+
+    'author': "Impulzia",
+    'website': "http://www.impulzia.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '11.0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    #'demo': [
+    #    'demo/demo.xml',
+    #],
+}
