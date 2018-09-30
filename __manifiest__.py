58ceb22555764af71c21dbe08059dcb4d3733cc7
Pedro A. Gracia Fajardo <lasarux@iMac-de-Pedro.local>
2018-09-30 16:38:46 +0200

    - add branch 11.0

--- __manifiest__.py
+++ __manifiest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '9.0.1',
+    'version': '11.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
