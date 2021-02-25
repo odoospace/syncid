79102101a4602465afe02d8c8550b1b868c1ae85
alfonsomb <alfonso@impulso.xyz>
2021-02-25 09:22:53 +0100

    'syncid' module migrated to 14.0

--- __manifest__.py
+++ __manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 {
     'name': "SyncID",
 
@@ -16,19 +15,14 @@
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '9.0.1',
+    'version': '13.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
     ],
-    # only loaded in demonstration mode
-    #'demo': [
-    #    'demo/demo.xml',
-    #],
 }
--- models/models.py
+++ models/models.py
@@ -1,21 +1,24 @@
-# -*- coding: utf-8 -*-
+from odoo import models, fields, api
 
-from openerp import models, fields, api
 
 class source(models.Model):
     _name = 'syncid.source'
+    _description = 'syncid source model'
 
     name = fields.Char()
 
+
 class reference(models.Model):
     _name = 'syncid.reference'
+    _description = 'syncid reference model'
 
     def object(self):
         # TODO: check this!
-        obj = selv.env[sel.model.name].browse(self.odoo_id)
+        obj = self.env[self.model.name].browse(self.odoo_id)
         return obj
 
     model = fields.Many2one('ir.model')
     source = fields.Many2one('syncid.source')
     odoo_id = fields.Integer()
-    source_id = fields.Char()
+    source_id = fields.Char(index=True)
+    scope = fields.Char(index=True)
