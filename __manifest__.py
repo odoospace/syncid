eb0adb75aa841465a898108d576a2c16f5790b49
alfonsomb <alfonso@impulso.xyz>
2021-02-25 09:24:08 +0100

    Version name to 14

--- __manifest__.py
+++ __manifest__.py
@@ -15,7 +15,7 @@
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '13.0.1',
+    'version': '14.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
